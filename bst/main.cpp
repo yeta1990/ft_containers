88cef6e1c8734fc15d015872986e308a84b12bc2
albgarci <albgarci@student.42madrid.com>
2023-01-30 12:55:54 +0100

    bst delete ok

--- bst/main.cpp
+++ bst/main.cpp
@@ -7,8 +7,24 @@ class Node;
 class Node{
 	public:
 		int value;
-		Node *left;
-		Node *right;
+		Node	*left;
+		Node	*right;
+		bool	hasAnyChild()
+		{
+			return (left || right);
+		}
+		Node	*hasOneChild()
+		{
+			if (left)
+				return (left);
+			else if (right)
+				return (right);
+			return (NULL);
+		}
+		bool	hasTwoChildren()
+		{
+			return (left && right);
+		}
 };
 
 class BSTree{
@@ -22,63 +38,122 @@ class BSTree{
 		Node	*createNode(int val)
 		{
 			Node* n;
+
 			n = new Node();
 			n->value = val;
 			n->left = NULL;
 			n->right= NULL;
-
 			return (n);
 		}
 
-		void insertFromRoot(int value, Node **root);
-		void freeTree(Node *root);
+		void	insertFromRoot(int value, Node **root);
+		Node	*del(int value, Node *root);
+		void	freeTree(Node *root);
 		Node	*root;
-		Node*	getNextLowestNode(Node *node);
-		Node*	findNode(int value, Node *prev, Node *parent);
+//		Node*	getNextLowestNode(Node *node);
+		Node*	getMaxNode(Node *node);
+		Node*	findNode(int value, Node *node);
 };
 
 void	BSTree::del(int value)
 {
-	Node	*parent;
-	Node	*found;
-	Node	*next;
+	this->del(value, root);
+}
 
-	parent = NULL;
-	found = findNode(value, this->root, parent);
+Node	*BSTree::del(int value, Node *root)
+{
+	Node	*maxNode;
+	Node	*aux;
+	Node	*child;
+
+	child = NULL;
+	aux = NULL;
+	maxNode = NULL;
+	if (root == NULL)
+		return (root);
+	else if (value < root->value)
+		root->left = del(value, root->left);
+	else if (value > root->value)
+		root->right = del(value, root->right);
+	else //found
+	{
+		std::cout << "found node: " << root->value << std::endl;
+		if (!root->hasAnyChild())
+		{
+			std::cout << "hasnt any child" << std::endl;
+			delete root;
+			root = NULL;
+		}
+		else if ((child = root->hasOneChild())) // go left
+		{
+			std::cout << "has one child" << std::endl;
+			aux = root;
+			root = child;
+			delete aux;
+		}
+		else if (root->hasTwoChildren())
+		{
+			maxNode = this->getMaxNode(root->right);
+			root->value = maxNode->value;
+			root->right = del(maxNode->value, root->right);
+			delete maxNode;
+		}
+		return (root);
+
+	}
+	return (root);
+/*
+	found = findNode(value, this->root);
+	maxNode = NULL;
+	child = NULL;
 	if (!found)
-		return ;	
-	next = getNextLowestNode(found);
+		return ;
+
+	std::cout << "found node: " << found->value << std::endl;
+	std::cout << "max next node is: " << getMaxNode(found)->value << std::endl;
+	if (!found->hasAnyChild()) //if hasn't children-> delete and = NULL
+	{
+		std::cout << "hasnt any child" << std::endl;
+//		delete found;
+		found = NULL;
+	}
+	else if ((child = found->hasOneChild()))
+	{
+		std::cout << "has one child" << std::endl;
+		found->value = child->value;
+		delete child;
+		child = NULL;
+	}
+	else if (found->hasTwoChildren())
+	{
+		std::cout << "has two children" << std::endl;
+		maxNode = getMaxNode(found);
+		found->value = maxNode->value;
+		delete maxNode;
+		maxNode = NULL;
+	}
+
+	//if has one child -> copy child and delete child
+	//if has two children -> find max value, copy and delete
+//	next = getNextLowestNode(found);
+*/
 }
 
-Node*	BSTree::getNextLowestNode(Node *node)
+Node*	BSTree::getMaxNode(Node *node)
 {
-	if (!node->left && node->right)
-		return (node->right);
-	else if (node->left && !node->right)
-		return (node->left);
-	else if (node->left->value < node->right->value)
-		return (node->left);
-	else if (node->right)
-		return (node->right);
-	return (NULL);
+	if (node->right)
+		return (getMaxNode(node->right));
+	return (node);
 }
 
-Node*	BSTree::findNode(int value, Node *node, Node *parent)
+Node*	BSTree::findNode(int value, Node *node)
 {
-	if (node->left && node->left->value == value)
-	{
-		parent = node;
-		return (node->left);
-	}
-	else if (node->left && node->right->value == value)
-	{
-		parent = node;
-		return (node->right);
-	}
-	else if (node->left)
-		return (findNode(value, node->left, node));
-	else if (node->right)
-		return (findNode(value, node->right, node));
+	if (node->value == value)
+		return (node);
+	else if (node->left && value <= node->value)
+		return (findNode(value, node->left));
+	else if (node->right && value >= node->value)
+		return (findNode(value, node->right));
 	return (NULL);
 }
 
@@ -141,7 +216,17 @@ int main()
 	tree->insert(-1);	
 	tree->insert(6);	
 	tree->insert(12);	
+	tree->insert(-7);
 	
+//	tree->del(-1);
+//	tree->del(1);
+	tree->del(3);
+	tree->del(2);
+//	tree->del(6);
+//	tree->del(12);
+//	tree->del(9);
+//	tree->del(-1);
+//	tree->del(8);
 
 	delete tree;
 
