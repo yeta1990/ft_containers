27fba6f01e9870f6ec689849e81dbb42ed2736aa
albgarci <albgarci@student.42madrid.com>
2023-03-05 00:56:46 +0100

    Fix: map operator <

--- map.hpp
+++ map.hpp
@@ -408,9 +408,13 @@ bool operator<( const ft::map<Key, T, Compare, Alloc>& lhs,
 
 	for (it = lhs.begin(), it2 = rhs.begin(); it != lhs.end() && it2 != rhs.end(); it++, it2++)
 	{
-		if (it->second < it2->second)
+		if (it->first < it2->first)
+			return (true);
+		else if (it->second < it2->second)
 			return (true);
 		else if (it->second > it2->second)
+			return (false);
+		else if (it->first > it2->first)
 			return (false);
 	}
 	return (lhs.size() < rhs.size());
@@ -442,7 +446,11 @@ bool operator>( const ft::map<Key, T, Compare, Alloc>& lhs,
 	{
 		if (it->second > it2->second)
 			return (true);
+		else if (it->first > it2->first)
+			return (true);
 		else if (it->second < it2->second)
+			return (false);
+		else if (it->first < it2->first)
 			return (false);
 	}
 	return (lhs.size() > rhs.size());
