3a8e87f113a37d77e3dfc02dc3735a83773b0e35
albgarci <albgarci@student.42madrid.com>
2023-03-09 09:27:30 +0100

    Vector: more performance tests

--- tests/benchmark_vector.cpp
+++ tests/benchmark_vector.cpp
@@ -39,8 +39,7 @@ void timer::reset()
 
 timer	t;
 
-unsigned long _ratio = 1000000;
-//unsigned long _ratio = 300;
+unsigned long _ratio = 100000;
 
 void	vec_b_insert()
 {
@@ -52,7 +51,7 @@ void	vec_b_insert()
 	tmp.assign(2600 * _ratio, 1);
 	vector.assign(4200 * _ratio, 7);
 
-	std::cout << "inserting " << tmp.size() << " elements in a vector of size " << vector.size() << std::endl;
+	std::cout << "inserting " << tmp.size() << " elements from a std vector in a ft vector of size " << vector.size() << std::endl;
     t = timer();
 	vector.insert(vector.begin(), tmp.begin(), tmp.end());
 	std::cout << "ft: " << t.get_time();
@@ -63,7 +62,6 @@ void	vec_b_insert()
 	v.push_back(vector.size());
 	v.push_back(vector.capacity());
 
-//	tmp.assign(2600 * _ratio, 1);
     t = timer();
 	vector2.assign(4200 * _ratio, 7);
 	vector2.insert(vector2.begin(), tmp.begin(), tmp.end());
@@ -75,8 +73,6 @@ void	vec_b_insert()
 	v.push_back(vector2.size());
 	v.push_back(vector2.capacity());
 
-//	return v;
-
 }
 
 void	vec_b_erase()
@@ -87,14 +83,14 @@ void	vec_b_erase()
     std::vector<int> v1;
 
     for (unsigned long i = 0; i < 9900 * _ratio; ++i)
+    {
         vector.push_back(i);
+    }
+	std::cout << "erasing the element " << (8 * _ratio) << "in a vector of size " << vector.size() << std::endl;
+
     t = timer();
-    
-//    vector.erase(vector.begin() + 8 * _ratio);
     v.push_back(*(vector.erase(vector.begin() + 8 * _ratio)));
-    
 	std::cout << "ft: " << t.get_time();
-//    g_end2 = timer();
     v.push_back(*(vector.begin() + 82 * _ratio));
     v.push_back(vector.size());
     v.push_back(vector.capacity());
@@ -118,18 +114,25 @@ void	vec_b_erase()
 
 void	vec_b_insert_heavy()
 {
-/*
+    t = timer();
     for (int i = 0; i < 2; ++i) {
         ft::vector<int> v;
 
-        for (std::size_t i = 0; i < MAXSIZE / 2; ++i) {
+        for (std::size_t i = 0; i < MAXSIZE / 3; ++i) {
             v.insert(v.end(), rand());
-            if (i % 100000 == 0)
-            	std::cout << i << std::endl;
         }
-//        BLOCK_OPTIMIZATION(v);
     }
-    */
+	std::cout << "ft: " << t.get_time();
+
+    t = timer();
+    for (int i = 0; i < 2; ++i) {
+        std::vector<int> v;
+
+        for (std::size_t i = 0; i < MAXSIZE / 3; ++i) {
+            v.insert(v.end(), rand());
+        }
+    }
+	std::cout << "std: " << t.get_time();
 }
 
 void	vec_b_erase_value()
@@ -138,39 +141,46 @@ void	vec_b_erase_value()
 	std::vector<int> vector1;
     std::vector<int> v;
    
-    for (unsigned int i = 0; i < 9900 * _ratio; ++i)
+	std::cout << "eooo" << std::endl;
+    for (unsigned int i = 0; i < 4000 * _ratio; ++i)
     {
         vector.push_back(i);
-        vector1.push_back(i);
+        v.push_back(i);
     }
-    
+
     t = timer();
-    v.push_back(*(vector.erase(vector.begin() + 8 * _ratio)));
+	for (unsigned int i = 0; i < 400; i++)
+	{
+		if (i % 2 == 0)
+			vector.erase(vector.begin() + i);
+		else 
+			vector.erase(vector.end() - i);
+	}
+	vector.insert(vector.begin() + 1000 * _ratio, v.begin() + 2000 * _ratio, v.begin() + 3500 * _ratio);
 	std::cout << "ft: " << t.get_time();
 
-    v.clear();
+    for (unsigned int i = 0; i < 4000 * _ratio; ++i)
+        vector1.push_back(i);
 
     t = timer();
-    v.push_back(*(vector1.erase(vector1.begin() + 8 * _ratio)));
+	for (unsigned int i = 0; i < 400; i++)
+	{
+		if (i % 2 == 0)
+			vector1.erase(vector1.begin() + 1);
+		else 
+			vector1.erase(vector1.end() - 1);
+	}
+	vector1.insert(vector1.begin() + 1000 * _ratio, v.begin() + 2000 * _ratio, v.begin() + 3500 * _ratio);
+
 	std::cout << "std: " << t.get_time();
-//    v.push_back(*(vector.begin() + 82 * _ratio));
-//    v.push_back(vector.size());
-//    v.push_back(vector.capacity());
-	
 }
 
-
-void	vec_b_swap()
-{
-
-}
 
 void	vector_benchmark()
 {
-//	test_case("vector: benchmark insert std iterators->", &vec_b_insert);
-//	test_case("vector: benchmark insert std iterators->", &vec_b_erase);
-//	test_case("vector: benchmark insert std iterators->", &vec_b_insert_heavy);
-	test_case("vector: benchmark erase std iterators->", &vec_b_erase_value);
-	test_case("vector: benchmark swap std iterators->", &vec_b_swap);
+	test_case("vector: benchmark insert std iterators->", &vec_b_insert);
+	test_case("vector: benchmark erase->", &vec_b_erase);
+	test_case("vector: benchmark insert heavy->", &vec_b_insert_heavy);
+	test_case("vector: benchmark erase value->", &vec_b_erase_value);
 
 }
