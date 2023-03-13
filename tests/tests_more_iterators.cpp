2eeac76e7d30ece1cfb82e175b2813fe0ef6995d
Alberto Garcia lopez <albgarci@student.42madrid.com>
2023-03-13 11:42:51 +0100

    Finished

--- tests/tests_more_iterators.cpp
+++ tests/tests_more_iterators.cpp
@@ -4,44 +4,7 @@
 #include "tests.hpp"
 #include <vector>
 #include "vector.hpp"
-#include "containers_test/srcs/base.hpp"
 
-void	ite_arrow()
-{
-	std::vector<foo<int> > vct(5);
-	std::vector<foo<int> >::iterator it(vct.begin());
-	std::vector<foo<int> >::const_iterator ite(vct.end());
-	ft::vector<foo<int> > vct1(5);
-	ft::vector<foo<int> >::iterator it1(vct1.begin());
-	ft::vector<foo<int> >::const_iterator ite1(vct1.end());
-
-	for (int i = 1; it != ite; ++i)
-		*it++ = i;
-	for (int i = 1; it1 != ite1; ++i)
-		*it1++ = i;
-
-	it = vct.begin();
-	it1 = vct1.begin();
-	for (int i = 1; it1 != ite1; ++i)
-	{
-		check(*it == *it1);
-		*it++ = i;
-		*it1++ = i;
-	}
-	it = vct.begin();
-	ite = vct.begin();
-	it1 = vct1.begin();
-	ite1 = vct1.begin();
-
-	check(*(++ite) == *(++ite1));
-	check(*(ite++) == *(ite1++));
-	check(*ite++ == *ite1++);
-	check(*++ite == *++ite1);
-
-//	check(it->m() == it1->m());
-//	check(ite->m() == ite1->m());
-
-}
 
 void	ite_vector()
 {
@@ -285,7 +248,6 @@ void	ite_end_begin()
 
 void	more_iterators()
 {
-	test_case("vector: more iterators", &ite_arrow);
 	test_case("vector: more more iterators", &ite_vector);
 //	test_case("vector: comparision operators", &comparision_operators);
 	test_case("vector: reverse iterator constructor", &rev_iterator_constructor);
