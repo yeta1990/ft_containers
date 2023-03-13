f4108c45f32f47df0b76818176d7459d362cded0
Alberto Garcia lopez <albgarci@student.42madrid.com>
2023-03-13 10:17:00 +0100

    2 new headers: iterator and type traits to ease testers work

---
+++ iterator.hpp
@@ -0,0 +1,10 @@
+#ifndef ITERATOR_HPP
+# define ITERATOR_HPP
+
+#include "iterator_traits.hpp"
+#include "reverse_iterator.hpp"
+#include "random_iterator.hpp"
+#include "tree_iterator.hpp"
+
+
+#endif
---
+++ type_traits.hpp
@@ -0,0 +1,6 @@
+#ifndef TYPE_TRAITS_HPP
+# define TYPE_TRAITS_HPP
+
+#include "is_integral.hpp"
+
+#endif
